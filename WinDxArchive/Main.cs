1eabf1d64ec26acf061ca3d40c936785a4ac1d0f
tk-xleader <tk-xleader@outlook.jp>
2025-08-11 11:28:43 +0900

    Add the application entrypoint.

---
+++ WinDxArchive/Main.cs
@@ -0,0 +1,16 @@
+/**
+ * WinDxArchive FileManager(Application entrypoint).
+ * Author;  tk-xleader.
+ * License: MIT License.
+ * */
+using System;
+using System.Windows.Forms;
+
+namespace WinDxArchive{
+	class FileManager{
+		/// Application entrypoint.
+		public static void Main(string[] args){
+			Application.Run(new Form());
+		}
+	}
+}
